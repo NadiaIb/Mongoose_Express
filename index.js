a375d742394b37dd6375bb294699b05eb7c932d6
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2024-06-25 19:10:42 +0100

    Details page using product id

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ async function main() {
   console.log("Mongo connection open");
 }
 
-
 app.get("/products", async (req, res) => {
   // returns thenable(promise like thing) so await response
-  const products = await Product.find({}) 
-  res.render("products/index", {products}); // looks into views/products/index, passes {products} info to ejs file
+  const products = await Product.find({});
+  res.render("products/index", { products }); // looks into views/products/index, passes {products} info to ejs file
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id); // findById = mongoose method
+  // console.log(product);
+  res.render("products/details", { product });
 });
 
 app.listen(3000, () => {
