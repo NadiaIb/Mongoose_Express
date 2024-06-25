5158d7a2f89df2b1488d30e68c9635373fbc9e1c
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2024-06-25 18:00:56 +0100

    Added mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require('mongoose');
+
+main().catch(err => console.log('Mongo connection error!', err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log("Mongo connection open");
+}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
