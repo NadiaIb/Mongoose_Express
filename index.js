0f054e1cc1101a3ef0b61209dfbd04fe20178671
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2024-06-27 17:39:34 +0100

    Filter by Category

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override");
 
 const Product = require("./models/product"); // require in product schema and model created
 
-const categories = Product.schema.obj.category.enum //gives list of categories, for the category select to be prefilled on edit page
+const categories = Product.schema.obj.category.enum; //gives list of categories, for the category select to be prefilled on edit page
 
 //MIDDLEWARE
 app.set("views", path.join(__dirname, "views"));
@@ -24,12 +24,18 @@ async function main() {
 
 app.get("/products", async (req, res) => {
   // returns thenable(promise like thing) so await response
-  const products = await Product.find({});
-  res.render("products/index", { products }); // looks into views/products/index, passes {products} info to ejs file
+  const { category } = req.query;
+  if(category){
+    const products = await Product.find({category});
+    res.render("products/index", { products, category }); // looks into views/products/index, passes {products} info to ejs file
+  }else{
+    const products = await Product.find({});
+    res.render("products/index", { products, category:'All' });
+  }
 });
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new",{categories});
+  res.render("products/new", { categories });
 });
 
 app.post("/products", async (req, res) => {
@@ -50,7 +56,7 @@ app.get("/products/:id", async (req, res) => {
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/edit", { product, categories});
+  res.render("products/edit", { product, categories });
 });
 
 app.put("/products/:id", async (req, res) => {
@@ -63,11 +69,11 @@ app.put("/products/:id", async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
-app.delete('/products/:id',async (req,res)=>{
+app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const deletedProduct= await Product.findByIdAndDelete(id)
-  res.redirect('/products')
-})
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
 
 app.listen(3000, () => {
   console.log("APP LISTENING ON PORT 30000");
